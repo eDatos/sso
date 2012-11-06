cce9dc49af3eddfdd8915ff369465913a31be5df
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-11-06 10:03:17 +0000

    METAMAC-1339 - Crear clase de utilidades que permita obtener el MetamacPrincipal y lanzar una excepción si una operación no está permitida http://jira.arte-consultores.com/browse/METAMAC-1339
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-sso/trunk/metamac-sso@41502 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-sso-client/src/main/java/org/siemac/metamac/sso/utils/SecurityUtils.java
@@ -0,0 +1,34 @@
+package org.siemac.metamac.statistical.resources.core.security;
+
+import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
+import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.core.common.exception.CommonServiceExceptionType;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.SsoClientConstants;
+
+public class SecurityUtils {
+
+    /**
+     * Retrieves MetamacPrincipal in ServiceContext
+     */
+    public static MetamacPrincipal getMetamacPrincipal(ServiceContext ctx) throws MetamacException {
+        Object principalProperty = ctx.getProperty(SsoClientConstants.PRINCIPAL_ATTRIBUTE);
+        if (principalProperty == null) {
+            throw new MetamacException(CommonServiceExceptionType.SECURITY_PRINCIPAL_NOT_FOUND);
+        }
+        MetamacPrincipal metamacPrincipal = (MetamacPrincipal) principalProperty;
+        if (!metamacPrincipal.getUserId().equals(ctx.getUserId())) {
+            throw new MetamacException(CommonServiceExceptionType.SECURITY_PRINCIPAL_NOT_FOUND);
+        }
+        return metamacPrincipal;
+    }
+    
+    
+    /**
+     * Throw an expection if operation not allowed
+     */
+    public static void throwExceptionIfOperationNotAllowed(ServiceContext ctx) throws MetamacException {
+        throw new MetamacException(CommonServiceExceptionType.SECURITY_OPERATION_NOT_ALLOWED, ctx.getUserId());
+    }
+
+}
