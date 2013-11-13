fc2d92d72d2c784853ee32d1b80d3749f46bcf08
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-13 12:46:25 +0000

    METAMAC-1969: Configuraciones de aplicaciones en Base de datos en vez de en archivo de propiedades http://jira.arte-consultores.com/browse/METAMAC-1969
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-sso/trunk/metamac-sso@49969 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/db/mssql/03-insert/01-configurations_insert_example.sql
@@ -0,0 +1,30 @@
+-- ###########################################
+-- # insert
+-- ###########################################
+
+-- URL LDAP Directory
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.ldap.url','FILL_ME_WITH_LDAP_URL');
+-- DN user to execute queries in LDAP Directory
+-- Sample: cn=admin,dc=mycompany,dc=com
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.ldap.user_dn','FILL_ME_WITH_DN_USER');
+-- Password of user than can execute queries in LDAP Directory
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.ldap.password','FILL_ME_WITH_USER_PASSWORD');
+-- Filter to execute queries to restrieve users
+-- Sample: uid=%u
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.ldap.filter','FILL_ME_WITH_USER_QUERY');
+-- DN base where users are located in LDAP Directory
+-- Sample: ou=users,dc=mycompany,dc=com
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.ldap.search_base',null);
+        
+-- DATASOURCE: MSSQL
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.datasource.url','jdbc:sqlserver://FILL_ME_WITH_HOST:FILL_ME_WITH_PORT;databaseName=DATABASE_NAME');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.datasource.username','FILL_ME_WITH_DB_USER');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.datasource.password','FILL_ME_WITH_DB_PASSWORD');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.datasource.driver_name','com.microsoft.sqlserver.jdbc.SQLServerDriver');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.security.datasource.query.attributes','SELECT usr.username as username, ''ACL'' as acl_name, ISNULL(app.code, '''') + ''#'' + ISNULL(rol.code, '''') + ''#'' + ISNULL(operations.code, '''') as acl_value
+        FROM TB_ACCESS acc
+        LEFT JOIN TB_USERS usr ON acc.USER_FK = usr.ID
+        LEFT JOIN TB_ROLES rol ON acc.ROLE_FK = rol.ID
+        LEFT JOIN TB_APPS app ON acc.APP_FK = app.ID
+        LEFT JOIN TB_EXTERNAL_ITEMS operations ON acc.OPERATION_FK = operations.ID
+        WHERE REMOVAL_DATE IS NULL AND {0}');
