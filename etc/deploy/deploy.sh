69b120c9e236f923199f45b77ea5c48602070cb7
Zebensui Méndez Corujo <zmencor@arte-consultores.com>
2016-08-10 11:50:07 +0100

    METAMAC-2475 - Migración de las aplicaciones de Tomcat6 a Tomcat8 y JDK
    - Deploy

---
+++ etc/deploy/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+HOME_PATH=metamac-sso-web
+TRANSFER_PATH=$HOME_PATH/tmp
+DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
+ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
+LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
+
+scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
+scp metamac-sso-web/target/authentication-service-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/authentication-service-internal.war
+ssh deploy@estadisticas.arte-consultores.com <<EOF
+
+    chmod a+x $TRANSFER_PATH/deploy/*.sh;
+    . $TRANSFER_PATH/deploy/utilities.sh
+
+    sudo service metamac01 stop
+    checkPROC "metamac"
+
+
+    ###
+    # AUTHENTICATION-SERVICE-INTERNAL
+    ###
+
+    # Update Process
+    sudo rm -rf $DEPLOY_TARGET_PATH/authentication-service-internal
+    sudo mv $TRANSFER_PATH/authentication-service-internal.war $DEPLOY_TARGET_PATH/authentication-service-internal.war
+    sudo unzip $DEPLOY_TARGET_PATH/authentication-service-internal.war -d $DEPLOY_TARGET_PATH/authentication-service-internal
+    sudo rm -rf $DEPLOY_TARGET_PATH/authentication-service-internal.war
+
+    # Restore Configuration
+    sudo mv $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/authentication-service-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo mv $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/authentication-service-internal/$LOGBACK_RELATIVE_PATH_FILE
+
+    sudo chown -R metamac.metamac /servers/metamac
+    sudo service metamac01 start
+    checkURL "http://estadisticas.arte-consultores.com/authentication-service-internal" "metamac01"
+
+EOF
---
+++ etc/deploy/utilities.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+info() {
+    echo "[ INFO  ] `date +'%Y/%m/%d %H:%M:%S'` -> $*"
+}
+error() {
+    echo "[ ERROR ] `date +'%Y/%m/%d %H:%M:%S'` -> $*"
+}
+
+MAXR=100
+SLEEP=3
+
+checkURL() {
+    URL=$1
+    NAME=$2
+    RESPONSE=`curl -sL -w "%{http_code}" "$URL" -o /dev/null`
+    I=0
+    while [ $RESPONSE -ne 200 ]; do
+        info "Waiting response from $NAME ($RESPONSE) $I/$MAXR ..."
+        sleep $SLEEP
+        I=$(( I + 1 ))
+        if [  $I -gt $MAXR ]; then
+            error "Timeout reached, $NAME not started properly ..."
+            # TODO: Send error by e-mail
+            break;
+        fi
+        RESPONSE=`curl -sL -w "%{http_code}" "$URL" -o /dev/null`
+    done
+}
+
+checkPROC() {
+     USER_SERVICE=$1
+
+     PID=`ps -fu $USER_SERVICE | tail -n +2 | awk '{printf $2" "}'`
+     I=0
+     while [ -n "$PID" ]; do
+         info "Stopping PID ($PID) $I/$MAXR..."
+         sleep $SLEEP
+         I=$(( I + 1 ))
+         if [  $I -gt $MAXR ]; then
+             error "Timeout reached, killing pid $PID  ..."
+             error "kill -9 $PID"
+             break;
+         fi
+         PID=`ps -fu $USER_SERVICE | tail -n +2 | awk '{printf $2" "}'`
+     done
+}
