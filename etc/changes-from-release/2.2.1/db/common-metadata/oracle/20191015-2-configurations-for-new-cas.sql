208b86058b8979684f1944921170ce8e1489ee9b
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2019-10-15 14:18:20 +0100

    EDATOS-3021 - Pasar a hacer uso del CAS de gobierno del vez del SSO
    > Para hacer el logout se ha de apuntar al CAS del logout delegado

--- etc/changes-from-release/2.2.1/db/common-metadata/oracle/20191015-2-configurations-for-new-cas.sql
+++ etc/changes-from-release/2.2.1/db/common-metadata/oracle/20191015-2-configurations-for-new-cas.sql
@@ -10,4 +10,7 @@ UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQU
 insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.security.datasource.dialect','org.hibernate.dialect.Oracle10gDialect');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
+-- El logout debe apuntar al logout del CAS delegado
+UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'FILL_ME' WHERE conf_key = 'metamac.security.cas_service_logout_url';
+-- Ejemplo: UPDATE TB_DATA_CONFIGURATIONS SET CONF_VALUE = 'https://demo-cas.arte-consultores.com/logout' WHERE conf_key = 'metamac.security.cas_service_logout_url';
 COMMIT;
